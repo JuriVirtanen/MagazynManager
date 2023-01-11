e834b89c5d384882ae7fd45c6fd74a77a199d03a
Gabriel Marczak <denetgabriel@gmail.com>
2023-01-11 17:41:51 +0100

    add colors

--- main.py
+++ main.py
@@ -3,20 +3,28 @@ import curses
 class item:
     def __init__(self, n, q, u, up, lista):
         self.name = n
-        self.quantity = float(q)
+        self.quantity = q
         self.unit = u
-        self.unit_price = float(up)
+        self.unit_price = up
         lista.append(self)
 
 def show(win, lista):
+    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    yellow = curses.color_pair(1)
+    cyan = curses.color_pair(2)
+    win.clear()
     win.addstr("Aktualne stany magazynowe :")
-    win.addstr(1, 0, "Nazwa")
-    win.addstr(1, 15, "Ilość")
-    win.addstr(1, 30, "Jednostka")
-    win.addstr(1, 45, "Cena za jednostkę")
+    win.addstr(1, 0, "Nazwa", cyan)
+    win.addstr(1, 25, "Ilość", cyan)
+    win.addstr(1, 50, "Jednostka", cyan)
+    win.addstr(1, 75, "Cena za jednostkę (PLN)", cyan)
     it = 0
     for a in lista:
-        win.addstr(it + 2, 0, a.name)
+        win.addstr(it + 2, 0, str(a.name), yellow)
+        win.addstr(it + 2, 25, str(a.quantity), yellow)
+        win.addstr(it + 2, 50, str(a.unit), yellow)
+        win.addstr(it + 2, 75, str(a.unit_price), yellow) 
         it += 1
     win.refresh()
 
@@ -35,9 +43,7 @@ def main(stdscr):
     show(stdscr, items)
     stdscr.getkey()
 
-
-
-
 stdscr = curses.initscr()
+curses.start_color()
 items = []
 curses.wrapper(main)
--- testy.py
+++ testy.py
@@ -6,7 +6,13 @@ class item:
         self.unit_price = float(up)
         lista.append(self)
 
+def show(lista):
+    print("Dostępne towary")
+    for a in lista:
+        print(a.name)
+
 items = []
 item1 = item("Ancient Fruit", 2, "szt.", "550", items)
-print(item1.name)
-print(items)
+item2 = item("Pineapple", 15, "szt.", "300", items)
+item3 = item("Coffee", 50, "szt.", "150", items)
+show(items)
