93f3d442a6ae810a564afadf4d15950a08791744
Gabriel Marczak <denetgabriel@gmail.com>
2023-01-11 16:42:05 +0100

    First successful run

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from curses import wrapper
+import curses
 
 class item:
     def __init__(self, n, q, u, up, lista):
@@ -8,16 +8,17 @@ class item:
         self.unit_price = float(up)
         lista.append(self)
 
-def show(stdscr, lista):
-    stdscr.addstr("Aktualne stany magazynowe :")
-    stdscr.addstr(1, 0, "Nazwa")
-    stdscr.addstr(1, 15, "Ilość")
-    stdscr.addstr(1, 30, "Jednostka")
-    stdscr.addstr(1, 45, "Cena za jednostkę")
-
+def show(win, lista):
+    win.addstr("Aktualne stany magazynowe :")
+    win.addstr(1, 0, "Nazwa")
+    win.addstr(1, 15, "Ilość")
+    win.addstr(1, 30, "Jednostka")
+    win.addstr(1, 45, "Cena za jednostkę")
+    it = 0
     for a in lista:
-        stdscr.addstr(2, 0, )
-
+        win.addstr(it + 2, 0, a.name)
+        it += 1
+    win.refresh()
 
 def add_item():
     pass
@@ -26,12 +27,17 @@ def sell_item():
     pass
 
 def main(stdscr):
+    item1 = item("Ancient Fruit", 2, "szt.", "550", items)
+    item2 = item("Pineapple", 15, "szt.", "300", items)
+    item3 = item("Coffee", 50, "szt.", "150", items)
     stdscr.clear()
     stdscr.refresh()
+    show(stdscr, items)
     stdscr.getkey()
 
 
 
-if __name__ == "__main__":
-    items = []
-    wrapper(main)
+
+stdscr = curses.initscr()
+items = []
+curses.wrapper(main)
